91ec0034ef0c1dd1c9e46e2ba6c52d1856f903be
talha.wajdan <talhawajdan@gmail.com>
2024-12-11 18:37:23 +0500

    update

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
   port: 8080,
-  dbUrl: "mongodb+srv://officetalha7:aJjklPCGTlKKcViV@chaeeapp.tqk6p.mongodb.net/",
+  dbUrl: "mongodb+srv://talhawajdan:lJZ1k7oHP1a1XOSX@chatapp.w8f1j.mongodb.net/",
   saltWorkFactor: 10,
   jwt_secret: "CHATEE_SECRET",
   cloudinary_cloud_name: "dcqieznto",
