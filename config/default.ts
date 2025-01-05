dc38ef59f391fa39e4f43685acd5d85e06032ccf
talha.wajdan <talhawajdan@gmail.com>
2025-01-05 12:18:18 +0500

    #update

--- config/default.ts
+++ config/default.ts
@@ -6,5 +6,5 @@ export default {
   cloudinary_cloud_name: "dcqieznto",
   cloudinary_api_key: "657821113681827",
   cloudinary_api_secret: "ivneFzUTOMcqPwVbpCOR5bIjqaU",
-  fe_url:"https://chatappfe-three.vercel.app/"
+  fe_url:"https://chatappfe-three.vercel.app"
 };
